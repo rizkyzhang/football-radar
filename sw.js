65ee6325fe419762c3bbf53165c6b2ed08bc51cc
rizkyzhang <rizkykho15@gmail.com>
2020-11-01 13:44:12 +0700

    feat: update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const STATIC_CACHE = `fbl-radar-static-v1`;
-const DYNAMIC_CACHE = `fbl-radar-dynamic-v1`;
+const STATIC_CACHE = "fbl-radar-static-v1";
+const DYNAMIC_CACHE = "fbl-radar-dynamic-v1";
 
-const assets = [
+const STATIC_ASSETS = [
   "/",
   "/index.html",
-  "/index.js",
   "/index.css",
+  "/index.js",
   "/materialize/materialize.min.css",
   "/materialize/materialize.min.js",
-  "/pages/Home.js",
-  "/pages/Team.js",
-  "/pages/Standing.js",
   "/api/fetchTeam.js",
   "/api/fetchStandings.js",
   "/api/fetchCompetitions.js",
+  "/pages/Home.js",
+  "/pages/Team.js",
+  "/pages/Standing.js",
+  "/pages/Favorite.js",
+  "/pages/Merchandise.js",
   "/db.js",
-  "/web_modules/vanilla-router.js",
   "/manifest.json",
   "/icons/app/favicon.ico",
   "/icons/app/favicon-16x16.png",
@@ -42,6 +43,18 @@ const assets = [
   "/icons/teams/FL1.png",
   "/icons/teams/PPL.png",
   "/icons/teams/not-found.svg",
+  "/images/mask.jpg",
+  "/images/ball1.jpg",
+  "/images/ball2.jpg",
+  "/images/ball3.jpg",
+  "/images/ball4.jpg",
+  "/images/shoe1.jpg",
+  "/images/shoe2.jpg",
+  "/images/shoe3.jpg",
+  "/images/shoe4.jpg",
+  "/images/shoe5.jpg",
+  "/images/shoe6.jpg",
+  "/images/shoe7.jpg",
 ];
 
 self.addEventListener("install", (event) => {
@@ -52,36 +65,32 @@ self.addEventListener("install", (event) => {
       .open(STATIC_CACHE)
       .then((cache) => {
         console.log("Caching assets");
-        cache.addAll(assets);
+        cache.addAll(STATIC_ASSETS);
       })
-      .then(() => self.skipWaiting()) // new service worker will be immediately activated
+      .then(() => self.skipWaiting()), // new service worker will be immediately activated
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches
+      .keys()
+      .then((keys) => Promise.all(
         keys
           .filter((key) => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
-          .map((key) => caches.delete(key))
-      );
-    })
+          .map((key) => caches.delete(key)),
+      )),
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cacheResponse) => {
-      return (
-        cacheResponse ||
-        fetch(event.request).then((fetchResponse) => {
-          return caches.open(DYNAMIC_CACHE).then((cache) => {
-            cache.put(event.request.url, fetchResponse.clone());
-            return fetchResponse;
-          });
-        })
-      );
-    })
+    caches.match(event.request).then(
+      (cacheResponse) => cacheResponse
+        || fetch(event.request).then((fetchResponse) => caches.open(DYNAMIC_CACHE).then((cache) => {
+          cache.put(event.request.url, fetchResponse.clone());
+          return fetchResponse;
+        })),
+    ),
   );
 });
