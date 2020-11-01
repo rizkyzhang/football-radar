859f11a086b4e378b1378fbb49b211caea91c53b
rizkyzhang <rizkykho15@gmail.com>
2020-11-01 15:40:03 +0700

    feat: update db.js

--- db.js
+++ db.js
@@ -6,37 +6,40 @@ const idbPromised = openDB("fbl-radar-db", 1, {
   },
 });
 
-export const addData = (store, data) => {
-  return new Promise((resolve, reject) => {
-    idbPromised
-      .then((db) => {
-        db.add(store, data);
-      })
-      .then(() => resolve(true))
-      .catch((error) => reject(error));
-  });
-};
+export const addData = (store, data) => new Promise((resolve, reject) => {
+  idbPromised
+    .then((db) => {
+      db.add(store, data);
+    })
+    .then(() => resolve(true))
+    .catch((error) => reject(error));
+});
 
-export const getAllData = (store) => {
-  return new Promise((resolve, reject) => {
-    idbPromised
-      .then((db) => {
-        const data = db.getAll(store);
-        return data;
-      })
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
-};
+export const getAllData = (store) => new Promise((resolve, reject) => {
+  idbPromised
+    .then((db) => {
+      const data = db.getAll(store);
+      return data;
+    })
+    .then((data) => resolve(data))
+    .catch((error) => reject(error));
+});
 
-export const getSingleData = (store, key) => {
-  return new Promise((resolve, reject) => {
-    idbPromised
-      .then((db) => {
-        const data = db.get(store, key);
-        return data;
-      })
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-  });
-};
+export const getSingleData = (store, key) => new Promise((resolve, reject) => {
+  idbPromised
+    .then((db) => {
+      const data = db.get(store, key);
+      return data;
+    })
+    .then((data) => resolve(data))
+    .catch((error) => reject(error));
+});
+
+export const deleteSingleData = (store, key) => new Promise((resolve, reject) => {
+  idbPromised
+    .then((db) => {
+      db.delete(store, key);
+    })
+    .then(() => resolve(true))
+    .catch((error) => reject(error));
+});
