e0165d2413a4032920a57b975cffc0616a68be38
rizkyzhang <rizkykho15@gmail.com>
2020-11-01 16:03:40 +0700

    feat(pages): update home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,8 +1,6 @@
 import { fetchCompetitions } from "../api/fetchCompetitions";
-import { addData, getAllData } from "../db.js";
 
-const buildHTML = (competition) => {
-  return ` 
+const buildHTML = (competition) => ` 
     <div class="col s12 m6 l4">
       <div class="card hoverable z-depth-2">
         <div class="card-image">
@@ -15,16 +13,15 @@ const buildHTML = (competition) => {
             <p>End: ${competition.currentSeason.endDate}</p>
         </div>
         <div class="card-action">
-            <a class="dynamic-link full-width btn light-blue waves-effect waves-light" href="/standings/${competition.id}">Detail</a>
+            <a href="/standings/${competition.id}" class="dynamic-link full-width btn blue lighten-1 waves-effect waves-light">Detail</a>
         </div>
       </div>
     </div>
   `;
-};
 
 const getHomeContent = async () => {
   const { competitions } = await fetchCompetitions();
-  let html = ``;
+  let html = "";
 
   competitions.forEach((competition) => {
     html += buildHTML(competition);
