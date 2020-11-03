9f64d41a10c6fdbfe0dbb00587dbe88878cef5e6
rizkyzhang <rizkykho15@gmail.com>
2020-11-03 22:08:26 +0700

    fix: navigation bug

--- index.js
+++ index.js
@@ -1,90 +1,15 @@
 import Router from "vanilla-router";
 import { addData, deleteSingleData, getSingleData } from "./db";
 
+import initServiceWorker from "./initServiceWorker";
 import getHomeContent from "./pages/Home";
 import getStandingContent from "./pages/Standing";
 import getTeamContent from "./pages/Team";
 import getFavoriteContent from "./pages/Favorite";
 import getMerchandiseContent from "./pages/Merchandise";
 
-const urlBase64ToUint8Array = (base64String) => {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-};
-
-const registerServiceWorker = () => {
-  navigator.serviceWorker
-    .register("./sw.js")
-    .then((registration) => console.log("Service worker is registered", registration))
-    .catch((error) => console.log("Service worker is not registered", error));
-};
-
-const requestPermission = () => {
-  if ("Notification" in window) {
-    Notification.requestPermission().then((result) => {
-      if (result === "denied") {
-        console.log("Permission is denied");
-        return;
-      }
-
-      if (result === "default") {
-        console.log("User close the dialog box");
-        return;
-      }
-
-      console.log("Permission is granted");
-    });
-  }
-
-  if ("PushManager" in window) {
-    navigator.serviceWorker.getRegistration().then((registration) => {
-      registration.pushManager
-        .subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(
-            "BC2772s1DVR40hrwya7iXEm_-Kry8Ion-DPqATHLk3l2pfF7RrB94kurlbBE4TilI1QnIWEqGKKloIOpVVNYeLw",
-          ),
-        })
-        .then((subscribe) => {
-          console.log("Subscribe is successful");
-          console.log(`Endpoint: ${subscribe.endpoint}`);
-          console.log(
-            `p256dh key: ${btoa(
-              String.fromCharCode.apply(
-                null,
-                new Uint8Array(subscribe.getKey("p256dh")),
-              ),
-            )}`,
-          );
-          console.log(
-            `auth key: ${btoa(
-              String.fromCharCode.apply(
-                null,
-                new Uint8Array(subscribe.getKey("auth")),
-              ),
-            )}`,
-          );
-        })
-        .catch((error) => console.error(`Subscribe is unsuccessful: ${error.message}`));
-    });
-  }
-};
-
-if ("serviceWorker" in navigator) {
-  registerServiceWorker();
-  requestPermission();
-} else {
-  console.error("Service worker is not supported in this browser");
-}
-
 const app = document.querySelector(".app");
-const links = document.querySelectorAll("a");
+const navLinks = document.querySelectorAll(".nav-link");
 
 const router = new Router({
   mode: "hash",
@@ -93,116 +18,54 @@ const router = new Router({
   },
 });
 
-router.add("", async () => {
-  app.innerHTML = `
-    <div class="container preloader-style">   
-      <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-          <div class="circle-clipper left">
-            <div class="circle"></div>
-          </div>
-          <div class="gap-patch">
-            <div class="circle"></div>
-          </div>
-          <div class="circle-clipper right">
-            <div class="circle"></div>
-          </div>
+const loadingCircle = `
+  <div class="container preloader-style">   
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue-only">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div>
+        <div class="gap-patch">
+          <div class="circle"></div>
+        </div>
+        <div class="circle-clipper right">
+          <div class="circle"></div>
         </div>
       </div>
     </div>
-  `;
+  </div>
+`;
+
+router.add("", async () => {
+  app.innerHTML = loadingCircle;
 
   const content = await getHomeContent();
   app.innerHTML = content;
 });
 
 router.add("standings/(:num)", async (id) => {
-  app.innerHTML = `
-    <div class="container preloader-style">   
-      <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-          <div class="circle-clipper left">
-            <div class="circle"></div>
-          </div>
-          <div class="gap-patch">
-            <div class="circle"></div>
-          </div>
-          <div class="circle-clipper right">
-            <div class="circle"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+  app.innerHTML = loadingCircle;
 
   const content = await getStandingContent(+id);
   app.innerHTML = content;
 });
 
 router.add("teams/(:num)", async (id) => {
-  app.innerHTML = `
-    <div class="container preloader-style">   
-      <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-          <div class="circle-clipper left">
-            <div class="circle"></div>
-          </div>
-          <div class="gap-patch">
-            <div class="circle"></div>
-          </div>
-          <div class="circle-clipper right">
-            <div class="circle"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+  app.innerHTML = loadingCircle;
 
   const content = await getTeamContent(+id);
   app.innerHTML = content;
 });
 
 router.add("favorite", async () => {
-  app.innerHTML = `
-    <div class="container preloader-style">   
-      <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-          <div class="circle-clipper left">
-            <div class="circle"></div>
-          </div>
-          <div class="gap-patch">
-            <div class="circle"></div>
-          </div>
-          <div class="circle-clipper right">
-            <div class="circle"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+  app.innerHTML = loadingCircle;
 
   const content = await getFavoriteContent();
   app.innerHTML = content;
 });
 
 router.add("merchandise", () => {
-  app.innerHTML = `
-    <div class="container preloader-style">   
-      <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue-only">
-          <div class="circle-clipper left">
-            <div class="circle"></div>
-          </div>
-          <div class="gap-patch">
-            <div class="circle"></div>
-          </div>
-          <div class="circle-clipper right">
-            <div class="circle"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
+  app.innerHTML = loadingCircle;
 
   const content = getMerchandiseContent();
   app.innerHTML = content;
@@ -212,11 +75,13 @@ router.addUriListener();
 router.check();
 
 document.addEventListener("DOMContentLoaded", () => {
+  initServiceWorker();
+  
   const sidenav = document.querySelector(".sidenav");
   M.Sidenav.init(sidenav);
 
-  links.forEach((link) => {
-    link.addEventListener("click", (event) => {
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (event) => {
       event.preventDefault();
       M.Sidenav.getInstance(sidenav).close();
       router.navigateTo(event.target.getAttribute("href"));
@@ -231,9 +96,7 @@ app.addEventListener("click", async (event) => {
   if (event.target.classList.contains("favorite-btn")) {
     const teamId = +window.location.href.split("/")[4];
     const teamIDB = await getSingleData("favorite", teamId);
-    const teamDecodedDataset = JSON.parse(
-      decodeURIComponent(event.target.dataset.team),
-    );
+    const teamDecodedDataset = JSON.parse(decodeURIComponent(event.target.dataset.team));
 
     if (!teamIDB) {
       addData("favorite", teamDecodedDataset);
@@ -249,9 +112,7 @@ app.addEventListener("click", async (event) => {
   }
 
   if (event.target.classList.contains("delete-btn")) {
-    const teamDecodedDataset = await JSON.parse(
-      decodeURIComponent(event.target.dataset.team),
-    );
+    const teamDecodedDataset = await JSON.parse(decodeURIComponent(event.target.dataset.team));
     const teamId = await teamDecodedDataset.id;
 
     if (teamId) {
